9d9cb6d9fea7cd349baece16b20f2b991c921199
waldir <waliuxd@gmail.com>
2025-02-06 17:59:17 -0500

    continuacion

--- session/settings.py
+++ session/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = environ.get("TIMEZONE")
 
 USE_I18N = True
 
