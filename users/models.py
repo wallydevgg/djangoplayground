31edacfed79de608214e51adf9f16914ae063b14
waldir <waliuxd@gmail.com>
2025-02-06 17:59:17 -0500

    continuacion

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    #overwritenn created fields
+    email = models.EmailField(unique=True)
+    avatar = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
+
+    REQUIRED_FIELDS = ['email']
