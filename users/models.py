c69c7e7931f684ab90b3165114fd68867933d34f
waldir-pc <waliuxd@gmail..com>
2025-02-07 06:06:39 -0500

    added swagger, redoc. custom model for <users>  etc:clase 2:30:00

--- users/models.py
+++ users/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
-    #overwritenn created fields
+    # overwritenn created fields
     email = models.EmailField(unique=True)
     avatar = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'users'
+        db_table = "users"
 
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ["email", "password"]
